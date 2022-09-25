bbfc27d90f3b5421c87d77d33fc2a4796a0a4bf7
IvanGer212 <comradeivan88@gmail.com>
2022-09-25 21:35:11 +0300

    Add new class Product

---
+++ spring-mvc/src/main/java/com/springmvclesson3/springmvc/domain/Product.java
@@ -0,0 +1,14 @@
+package com.springmvclesson3.springmvc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private long id;
+    private String title;
+    private double cost;
+}
