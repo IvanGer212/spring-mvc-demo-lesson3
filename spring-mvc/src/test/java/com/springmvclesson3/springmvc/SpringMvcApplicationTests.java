4f58621fd77abcd3574ffb9739d27cc219676fef
IvanGer212 <comradeivan88@gmail.com>
2022-09-25 21:30:38 +0300

    Initial commit

---
+++ spring-mvc/src/test/java/com/springmvclesson3/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvclesson3.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
