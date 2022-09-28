6942b4d5b08c304f1da89a89a4f1449a2f413f9e
IvanGer212 <comradeivan88@gmail.com>
2022-09-28 21:44:29 +0300

    Add class for exception handling, add new method in controller for add new Product and redirect to exception page or to product page.

--- spring-mvc/src/main/java/com/springmvclesson3/springmvc/controller/ProductController.java
+++ spring-mvc/src/main/java/com/springmvclesson3/springmvc/controller/ProductController.java
@@ -1,16 +1,16 @@
 package com.springmvclesson3.springmvc.controller;
 
 import com.springmvclesson3.springmvc.domain.Product;
+import com.springmvclesson3.springmvc.exception.ErrorResponse;
 import com.springmvclesson3.springmvc.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/product")
@@ -30,5 +30,36 @@ public class ProductController {
         return productService.findByID(id).get();
     }
 
+    @GetMapping("/create-product")
+    public String createAddProductPage(Model model){
+        model.addAttribute("newProduct", new Product());
 
+        return "create-product";
+    }
+    @PostMapping
+    public String addProduct(@ModelAttribute("newProduct") Product newProduct, Model model) {
+        Optional<ErrorResponse> validationError = productValidation(newProduct);
+        if(validationError.isPresent()){
+            model.addAttribute("error", validationError.get());
+            return "exception-page";
+        }
+        productService.create(newProduct);
+        return "redirect:/product";
+    }
+
+    private Optional<ErrorResponse> productValidation(Product product) {
+        List<String> details = new ArrayList<>();
+
+        if (product.getTitle().isEmpty()) {
+            details.add("Product name cannot be empty.");
+        }
+        if (product.getCost() <= 0) {
+            details.add("Price cannot be less then 0.");
+        }
+        if(!details.isEmpty()){
+            return Optional.of(new ErrorResponse("Validation failed!", details));
+        }
+
+        return Optional.empty();
+    }
 }
